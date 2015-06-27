eaa0b7227cdeb5a9a44a96e18599ebf74d480e0c
Mishbah Razzaque <mishbah@jp74.com>
2015-06-27 01:53:48 +0100

    Remove custom map styles.

--- djangocms_gmaps/static/js/djangocms_gmaps/coordinates_widget.js
+++ djangocms_gmaps/static/js/djangocms_gmaps/coordinates_widget.js
@@ -5,83 +5,7 @@
             coordinatesInput = $('input[name="coordinates"]'),
             addressComponents = $(
                 'input[name="street_address"], input[name="locality"], input[name="region"], input[name="postal_code"], select[name="country_short"]'
-            ),
-            styles = [{
-                "featureType": "landscape",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 65
-                }, {
-                    "visibility": "on"
-                }]
-            }, {
-                "featureType": "poi",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 51
-                }, {
-                    "visibility": "simplified"
-                }]
-            }, {
-                "featureType": "road.highway",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "visibility": "simplified"
-                }]
-            }, {
-                "featureType": "road.arterial",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 30
-                }, {
-                    "visibility": "on"
-                }]
-            }, {
-                "featureType": "road.local",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 40
-                }, {
-                    "visibility": "on"
-                }]
-            }, {
-                "featureType": "transit",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "visibility": "simplified"
-                }]
-            }, {
-                "featureType": "administrative.province",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            }, {
-                "featureType": "water",
-                "elementType": "labels",
-                "stylers": [{
-                    "visibility": "on"
-                }, {
-                    "lightness": -25
-                }, {
-                    "saturation": -100
-                }]
-            }, {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "hue": "#ffff00"
-                }, {
-                    "lightness": -25
-                }, {
-                    "saturation": -97
-                }]
-            }];
+            );
 
         var addressOnChange = function() {
             var address = addressComponents.map(function() {
@@ -133,7 +57,7 @@
             var mapOptions = {
                 center: new google.maps.LatLng(40.712784, -74.005941),
                 zoom: 11,
-                styles: styles
+                mapTypeControl: false,
             };
             map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
             if (coordinatesInput.val()) {
